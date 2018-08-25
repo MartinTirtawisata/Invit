310265e43ac16d97e70d04a164e914a5259c5d16
martintirtawisata <martin.tirtawisata@hotmail.com>
2018-08-24 21:07:35 -0700

    added npm

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
+'use strict'
+
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
+
 app.use(express.static('public'));
+
+let server;
+
+function runServer(database)
+
 app.listen(process.env.PORT || 8080);
---
+++ test/test.js
@@ -0,0 +1,14 @@
+// const chai = require('chai');
+// const chaiHttp = require('chai-http');
+// const expect = chai.expect;
+// chai.use(chaiHttp);
+
+// const {app} = require('../server');
+
+// describe("test for status code", function(){
+//     it('should show 200 status code when getting', function(){
+//         return chai.request(app).get('/home').then(res => {
+//             expect(res).to.have.status(200);
+//         })
+//     })
+// })
